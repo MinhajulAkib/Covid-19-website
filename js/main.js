e7bb90c00fc02cbaf2d6337ec9c7555294f880fd
MinhajulAkib <00akibswe@gmail.com>
2021-07-30 22:22:27 +0600

    navbar section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+        $("button").click(function(){
+             $(".demo1").fadeIn();
+        })
+        $("button").click(function(){
+            $(".demo2").fadeIn(900);
+        })
+        $("button").click(function(){
+            $(".demo3").fadeIn(1800);
+        })
+
+
+
+})
