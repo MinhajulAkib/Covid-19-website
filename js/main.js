3562b2dfa91b6814509e952849e0b468a471e29a
MinhajulAkib <00akibswe@gmail.com>
2021-08-02 16:56:46 +0600

    home section responsive

--- js/main.js
+++ js/main.js
@@ -3,13 +3,4 @@ $(document).ready(function(){
              $(this).toggleClass('fa-times');
              $('.navbar').toggleClass('nav-toggle');
         })
-        $("button").click(function(){
-            $(".demo2").fadeIn(900);
-        })
-        $("button").click(function(){
-            $(".demo3").fadeIn(1800);
-        })
-
-
-
 })
